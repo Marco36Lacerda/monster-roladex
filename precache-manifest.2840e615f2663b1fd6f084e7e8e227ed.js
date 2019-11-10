d3cc35a17bf4dc9b849e1ddc114d85e060c6759a
MRODRIGUES <mrodrigues@cartrawler.com>
2019-11-10 19:33:53 +0000

    Updates

---
+++ precache-manifest.2840e615f2663b1fd6f084e7e8e227ed.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "6e06bd13861b4a5b0cd43be445c39841",
+    "url": "/monster-roladex/index.html"
+  },
+  {
+    "revision": "ed448f8c361e7d2f923e",
+    "url": "/monster-roladex/static/css/main.faab542a.chunk.css"
+  },
+  {
+    "revision": "c4de08c8e1140d25c0ae",
+    "url": "/monster-roladex/static/js/2.5905bb17.chunk.js"
+  },
+  {
+    "revision": "ed448f8c361e7d2f923e",
+    "url": "/monster-roladex/static/js/main.1b4c81b3.chunk.js"
+  },
+  {
+    "revision": "e62bfa2bd48a9cbe5017",
+    "url": "/monster-roladex/static/js/runtime-main.b4165ffb.js"
+  }
+]);
